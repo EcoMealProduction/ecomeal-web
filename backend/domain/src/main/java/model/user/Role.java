b72e613334c5280f1848b3fcf3c4bfe00e4d9cb2
DanCh11 <dan.chiriac1453@gmail.com>
2025-05-06 16:32:25 +0200

    added user roles

--- backend/domain/src/main/java/model/user/Role.java
+++ backend/domain/src/main/java/model/user/Role.java
@@ -1,4 +1,16 @@
 package model.user;
 
+/**
+ * Defines user roles in the system.
+ */
 public enum Role {
+    /**
+     * Regular user buying surplus food.
+     */
+    CLIENT,
+
+    /**
+     * Business user offering surplus food
+     */
+    RESTAURANT
 }
