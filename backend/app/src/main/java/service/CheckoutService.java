e014dfbd8311aaf41aec85c28268dc53d3270b10
DanCh11 <dan.chiriac1453@gmail.com>
2025-05-17 17:41:00 +0200

    added methods names for checkout service

--- backend/app/src/main/java/service/CheckoutService.java
+++ backend/app/src/main/java/service/CheckoutService.java
@@ -1,4 +1,35 @@
 package service;
 
-public class CheckoutService {
+import in.CheckoutUseCase;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import out.CartRepository;
+import out.ClientRepository;
+
+@Service
+@AllArgsConstructor
+public class CheckoutService implements CheckoutUseCase {
+
+    private ClientRepository clientRepository;
+    private CartRepository cartRepository;
+
+    @Override
+    public void checkout(long clientId) {
+
+    }
+
+    @Override
+    public void cancelCheckout(long clientId) {
+
+    }
+
+    @Override
+    public void confirmPayment(long cartId) {
+
+    }
+
+    @Override
+    public void rollbackCheckout(long cartId) {
+
+    }
 }
